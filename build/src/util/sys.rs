45997617c667ec0678b32ac91f084b0177824bf8
renoseven <dev@renoseven.net>
2022-11-17 14:15:45 +0800

    sys: implement get process id, path, name
    Signed-off-by: renoseven <dev@renoseven.net>

--- build/src/util/sys.rs
+++ build/src/util/sys.rs
@@ -1,18 +1,66 @@
 use super::fs;
 
-pub fn get_cpu_num() -> std::io::Result<usize> {
-    const SYS_INFO_ONLINE_CPU_FILE_PATH: &str = "/sys/devices/system/cpu/online";
-    const SYS_INFO_SPLITER: char = '-';
+const PROC_SELF_PATH:     &str = "/proc/self";
+const PROC_SELF_EXE_PATH: &str = "/proc/self/exe";
 
-    fs::check_file(SYS_INFO_ONLINE_CPU_FILE_PATH)?;
+lazy_static::lazy_static! {
+    static ref PROCESS_ID:   u32    = SysInitializer::init_process_id();
+    static ref PROCESS_PATH: String = SysInitializer::init_process_path();
+    static ref PROCESS_NAME: String = SysInitializer::init_process_name();
+    static ref CPU_NUM:      usize  = SysInitializer::init_cpu_num();
+}
 
-    let cpu_online_info = fs::read_file_to_string(SYS_INFO_ONLINE_CPU_FILE_PATH)?;
-    let max_cpu_id = cpu_online_info
-        .split(SYS_INFO_SPLITER)
-        .last()
-        .unwrap_or_default()
-        .parse::<usize>()
-        .unwrap_or_default();
+struct SysInitializer;
 
-    Ok(max_cpu_id + 1) // cpu id start from 0
+impl SysInitializer {
+    pub fn init_process_id() -> u32 {
+        fs::stringtify_path(
+            std::fs::read_link(PROC_SELF_PATH).expect("Get process id failed")
+        ).parse::<u32>().expect("Parse process id failed")
+    }
+
+    pub fn init_cpu_num() -> usize {
+        const SYS_INFO_SPLITER: char = '-';
+        const SYS_INFO_ONLINE_CPU_FILE_PATH: &str = "/sys/devices/system/cpu/online";
+
+        let cpu_online_info = fs::read_file_to_string(SYS_INFO_ONLINE_CPU_FILE_PATH).expect("Read cpu number failed");
+        let max_cpu_id = cpu_online_info
+            .split(SYS_INFO_SPLITER)
+            .last()
+            .unwrap_or_default()
+            .parse::<usize>()
+            .unwrap_or_default();
+
+        // cpu id start from 0
+        max_cpu_id + 1
+    }
+
+    pub fn init_process_path() -> String {
+        fs::stringtify_path(
+            std::fs::read_link(PROC_SELF_EXE_PATH).expect("Read process id failed")
+        )
+    }
+
+    pub fn init_process_name() -> String {
+        fs::stringtify_path(
+            std::fs::read_link(PROC_SELF_EXE_PATH).expect("Read process name failed")
+                .file_name().expect("Parse process name failed")
+        )
+    }
+}
+
+pub fn get_process_id() -> u32 {
+    *PROCESS_ID
+}
+
+pub fn get_process_path() -> &'static str {
+    PROCESS_PATH.as_str()
+}
+
+pub fn get_process_name() -> &'static str {
+    PROCESS_NAME.as_str()
+}
+
+pub fn get_cpu_num() -> usize {
+    *CPU_NUM
 }
