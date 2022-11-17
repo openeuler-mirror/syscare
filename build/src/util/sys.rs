09c155f53954f564374cde271d2ecf898e82d28c
renoseven <dev@renoseven.net>
2022-11-17 15:23:08 +0800

    sys: remove constants
    Signed-off-by: renoseven <dev@renoseven.net>

--- build/src/util/sys.rs
+++ build/src/util/sys.rs
@@ -1,8 +1,5 @@
 use super::fs;
 
-const PROC_SELF_PATH:     &str = "/proc/self";
-const PROC_SELF_EXE_PATH: &str = "/proc/self/exe";
-
 lazy_static::lazy_static! {
     static ref PROCESS_ID:   u32    = SysInitializer::init_process_id();
     static ref PROCESS_PATH: String = SysInitializer::init_process_path();
@@ -15,35 +12,33 @@ struct SysInitializer;
 impl SysInitializer {
     pub fn init_process_id() -> u32 {
         fs::stringtify_path(
-            std::fs::read_link(PROC_SELF_PATH).expect("Get process id failed")
+            std::fs::read_link("/proc/self").expect("Get process id failed")
         ).parse::<u32>().expect("Parse process id failed")
     }
 
     pub fn init_cpu_num() -> usize {
-        const SYS_INFO_SPLITER: char = '-';
-        const SYS_INFO_ONLINE_CPU_FILE_PATH: &str = "/sys/devices/system/cpu/online";
+        let cpu_online_info = fs::read_file_to_string("/sys/devices/system/cpu/online")
+            .expect("Read cpu number failed");
 
-        let cpu_online_info = fs::read_file_to_string(SYS_INFO_ONLINE_CPU_FILE_PATH).expect("Read cpu number failed");
         let max_cpu_id = cpu_online_info
-            .split(SYS_INFO_SPLITER)
+            .split('-')
             .last()
             .unwrap_or_default()
             .parse::<usize>()
-            .unwrap_or_default();
+            .unwrap_or_default() + 1; // cpu id start from 0
 
-        // cpu id start from 0
-        max_cpu_id + 1
+        max_cpu_id
     }
 
     pub fn init_process_path() -> String {
         fs::stringtify_path(
-            std::fs::read_link(PROC_SELF_EXE_PATH).expect("Read process id failed")
+            std::fs::read_link("/proc/self/exe").expect("Read process id failed")
         )
     }
 
     pub fn init_process_name() -> String {
         fs::stringtify_path(
-            std::fs::read_link(PROC_SELF_EXE_PATH).expect("Read process name failed")
+            std::fs::read_link("/proc/self/exe").expect("Read process name failed")
                 .file_name().expect("Parse process name failed")
         )
     }
