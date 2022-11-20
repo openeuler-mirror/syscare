51cb39209d43c3ce1a33dcaef722d88543e2d4c1
renoseven <dev@renoseven.net>
2022-11-20 11:58:30 +0800

    constants: remove useless consts
    Signed-off-by: renoseven <dev@renoseven.net>

--- build/src/constants.rs
+++ build/src/constants.rs
@@ -19,7 +19,6 @@ pub const PATCH_FILE_EXTENSION:  &str  = "patch";
 pub const PATCH_FILE_PERMISSION: &str  = "640";
 pub const PATCH_DIR_PERMISSION:  &str  = "750";
 pub const PATCH_INSTALL_PATH:    &str  = "/usr/lib/syscare/patches";
-pub const PATCH_UNDEFINED_VALUE: &str  = "undefined";
 pub const PATCH_INFO_FILE_NAME:  &str  = "patch_info";
 pub const PATCH_VERSION_DIGITS:  usize = 8;
 
@@ -31,7 +30,6 @@ pub const PKG_FLAG_SOURCE_PKG:              &str = "(none)";
 pub const PKG_PATCH_VERSION_FILE_NAME:      &str = "syscare-patch-version";
 pub const PKG_PATCH_TARGET_FILE_NAME:       &str = "syscare-patch-target";
 pub const PKG_SPEC_FILE_EXTENSION:          &str = "spec";
-pub const PKG_SPEC_TAG_SPLITER:             char = ':';
 pub const PKG_SPEC_TAG_NAME_RELEASE:        &str = "Release:";
 pub const PKG_SPEC_TAG_NAME_SOURCE:         &str = "Source";
 pub const PKG_SPEC_TAG_NAME_BUILD_REQUIRES: &str = "BuildRequires:";
@@ -39,7 +37,6 @@ pub const PKG_SPEC_TAG_VALUE_GROUP:         &str = "Patch";
 
 // Kernel
 pub const KERNEL_PKG_NAME:          &str = "kernel";
-pub const KERNEL_SOURCE_DIR_FLAG:   &str = "Kbuild";
 pub const KERNEL_SOURCE_DIR_PREFIX: &str = "linux-";
 pub const KERNEL_CONFIG_NAME:       &str = ".config";
 pub const KERNEL_DEFCONFIG_NAME:    &str = "openeuler_defconfig";
