b478dfcf9788b48ec792e34458478058a3a82c3d
renoseven <dev@renoseven.net>
2022-12-07 01:23:22 +0800

    build: fix 'version flag would lost when env is not set' issue
    Signed-off-by: renoseven <dev@renoseven.net>

--- build/build.rs
+++ build/build.rs
@@ -2,6 +2,9 @@ const ENV_VERSION_NAME: &str = "SYSCARE_BUILD_VERSION";
 
 fn main() {
     if let Ok(value) = std::env::var(ENV_VERSION_NAME) {
+        if value.is_empty() {
+            return;
+        }
         println!("cargo:rustc-env=CARGO_PKG_VERSION={}", value);
     }
 }
