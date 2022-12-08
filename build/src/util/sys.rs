1723e36a398cba35a5fb78e1b1285dc46449b1b3
renoseven <dev@renoseven.net>
2022-12-09 00:16:59 +0800

    build: support detect system architecture
    Signed-off-by: renoseven <dev@renoseven.net>

--- build/src/util/sys.rs
+++ build/src/util/sys.rs
@@ -3,21 +3,14 @@ use lazy_static::*;
 use super::fs;
 
 lazy_static! {
-    static ref PROCESS_ID:   u32    = SysInitializer::init_process_id();
+    static ref CPU_NUM:      usize  = SysInitializer::init_cpu_num();
     static ref PROCESS_PATH: String = SysInitializer::init_process_path();
     static ref PROCESS_NAME: String = SysInitializer::init_process_name();
-    static ref CPU_NUM:      usize  = SysInitializer::init_cpu_num();
 }
 
 struct SysInitializer;
 
 impl SysInitializer {
-    pub fn init_process_id() -> u32 {
-        fs::stringtify(
-            std::fs::read_link("/proc/self").expect("Get process id failed")
-        ).parse::<u32>().expect("Parse process id failed")
-    }
-
     pub fn init_cpu_num() -> usize {
         let cpu_online_info = fs::read_file_to_string("/sys/devices/system/cpu/online")
             .expect("Read cpu number failed");
@@ -35,7 +28,7 @@ impl SysInitializer {
     pub fn init_process_path() -> String {
         fs::stringtify(
             std::fs::read_link("/proc/self/exe")
-                .expect("Read process id failed")
+                .expect("Read process path failed")
         )
     }
 
@@ -47,8 +40,16 @@ impl SysInitializer {
     }
 }
 
+pub fn get_cpu_arch() -> &'static str {
+    std::env::consts::ARCH
+}
+
+pub fn get_cpu_num() -> usize {
+    *CPU_NUM
+}
+
 pub fn get_process_id() -> u32 {
-    *PROCESS_ID
+    std::process::id()
 }
 
 pub fn get_process_path() -> &'static str {
@@ -57,8 +58,4 @@ pub fn get_process_path() -> &'static str {
 
 pub fn get_process_name() -> &'static str {
     PROCESS_NAME.as_str()
-}
-
-pub fn get_cpu_num() -> usize {
-    *CPU_NUM
 }
