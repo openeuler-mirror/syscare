52932f8a03b18ebce4aa3e00d8b642ef48ed706a
snoweay <snoweay@163.com>
2023-06-08 03:30:05 +0000

    cli: fix lint format in src/boot
    Signed-off-by: snoweay <snoweay@163.com>

--- cli/src/boot/manager.rs
+++ cli/src/boot/manager.rs
@@ -1,11 +1,11 @@
 use std::ffi::OsStr;
-use std::{path::PathBuf, ffi::OsString};
+use std::{ffi::OsString, path::PathBuf};
 
 use log::debug;
 
 use common::os;
-use common::util::os_str::OsStringExt;
 use common::util::fs;
+use common::util::os_str::OsStringExt;
 
 pub enum RebootOption {
     Normal,
@@ -13,7 +13,7 @@ pub enum RebootOption {
 }
 
 struct LoadKernelOption {
-    name:   OsString,
+    name: OsString,
     kernel: PathBuf,
     initrd: PathBuf,
 }
@@ -22,9 +22,9 @@ pub struct BootManager;
 
 impl BootManager {
     fn find_kernel_by_name<S: AsRef<OsStr>>(name: S) -> std::io::Result<LoadKernelOption> {
-        const BOOT_DIR_NAME:       &str = "/boot";
-        const KERNEL_PREFIX:       &str = "vmlinuz-";
-        const INITRAMFS_PREFIX:    &str = "initramfs-";
+        const BOOT_DIR_NAME: &str = "/boot";
+        const KERNEL_PREFIX: &str = "vmlinuz-";
+        const INITRAMFS_PREFIX: &str = "initramfs-";
         const INITRAMFS_EXTENSION: &str = ".img";
 
         debug!("Finding kernel {:?}", name.as_ref());
@@ -32,12 +32,20 @@ impl BootManager {
         let kernel = fs::find_file(
             &boot_dir,
             OsString::from(KERNEL_PREFIX).concat(&name),
-            fs::FindOptions { fuzz: false, recursive: false }
+            fs::FindOptions {
+                fuzz: false,
+                recursive: false,
+            },
         )?;
         let initrd = fs::find_file(
             &boot_dir,
-            OsString::from(INITRAMFS_PREFIX).concat(&name).concat(INITRAMFS_EXTENSION),
-            fs::FindOptions { fuzz: false, recursive: false }
+            OsString::from(INITRAMFS_PREFIX)
+                .concat(&name)
+                .concat(INITRAMFS_EXTENSION),
+            fs::FindOptions {
+                fuzz: false,
+                recursive: false,
+            },
         )?;
 
         Ok(LoadKernelOption {
@@ -52,7 +60,7 @@ impl BootManager {
         let entry = os::grub::get_boot_entry()?;
 
         Ok(LoadKernelOption {
-            name:   entry.get_name().to_os_string(),
+            name: entry.get_name().to_os_string(),
             kernel: entry.get_kernel(),
             initrd: entry.get_initrd(),
         })
@@ -60,17 +68,13 @@ impl BootManager {
 
     pub fn load_kernel<S: AsRef<OsStr>>(kernel_version: Option<S>) -> std::io::Result<()> {
         let option = match kernel_version {
-            Some(version) => {
-                Self::find_kernel_by_name(version)
-            },
-            None => {
-                Self::find_kernel_by_grub_config().or_else(|e| {
-                    debug!("{}", e);
-                    Self::find_kernel_by_name(os::kernel::version())
-                })
-            },
+            Some(version) => Self::find_kernel_by_name(version),
+            None => Self::find_kernel_by_grub_config().or_else(|e| {
+                debug!("{}", e);
+                Self::find_kernel_by_name(os::kernel::version())
+            }),
         }?;
-        debug!("Loading {:?}",       option.name);
+        debug!("Loading {:?}", option.name);
         debug!("Using kernel: {:?}", option.kernel);
         debug!("Using initrd: {:?}", option.initrd);
 
