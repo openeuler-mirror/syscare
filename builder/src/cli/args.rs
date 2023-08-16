0157487c776d039d30136c13f963237a71b19891
renoseven <dev@renoseven.net>
2023-08-16 14:34:00 +0800

    syscare: initialize project MSRV 1.64
    Signed-off-by: renoseven <dev@renoseven.net>

--- builder/src/cli/args.rs
+++ builder/src/cli/args.rs
@@ -15,65 +15,66 @@ const DEFAULT_OUTPUT_DIR: &str = ".";
 
 lazy_static! {
     static ref DEFAULT_BUILD_JOBS: String = os::cpu::num().to_string();
+    static ref DEFAULT_PATCH_ARCH: String = os::cpu::arch().to_string_lossy().to_string();
 }
 
 #[derive(Parser, Debug)]
-#[command(bin_name=PatchBuildCLI::name(), version=PatchBuildCLI::version())]
+#[clap(bin_name=PatchBuildCLI::name(), version=PatchBuildCLI::version())]
 pub struct CliArguments {
     /// Patch name
-    #[arg(short = 'n', long)]
+    #[clap(short = 'n', long)]
     pub patch_name: String,
 
     /// Patch architecture
-    #[arg(long, default_value=os::cpu::arch())]
+    #[clap(long, default_value=DEFAULT_PATCH_ARCH.as_str())]
     pub patch_arch: String,
 
     /// Patch version
-    #[arg(long, default_value=DEFAULT_PATCH_VERSION)]
+    #[clap(long, default_value=DEFAULT_PATCH_VERSION)]
     pub patch_version: String,
 
     /// Patch release
-    #[arg(long, default_value=DEFAULT_PATCH_RELEASE)]
+    #[clap(long, default_value=DEFAULT_PATCH_RELEASE)]
     pub patch_release: u32,
 
     /// Patch description
-    #[arg(long, default_value=DEFAULT_PATCH_DESCRIPTION)]
+    #[clap(long, default_value=DEFAULT_PATCH_DESCRIPTION)]
     pub patch_description: String,
 
     /// Source package
-    #[arg(short, long)]
+    #[clap(short, long)]
     pub source: PathBuf,
 
     /// Debuginfo package(s)
-    #[arg(short, long, required = true)]
+    #[clap(short, long, required = true)]
     pub debuginfo: Vec<PathBuf>,
 
     /// Working directory
-    #[arg(long, default_value=DEFAULT_WORK_DIR)]
+    #[clap(long, default_value=DEFAULT_WORK_DIR)]
     pub workdir: PathBuf,
 
     /// Generated patch output directory
-    #[arg(short, long, default_value=DEFAULT_OUTPUT_DIR)]
+    #[clap(short, long, default_value=DEFAULT_OUTPUT_DIR)]
     pub output: PathBuf,
 
     /// Parallel build jobs
-    #[arg(short, long, value_name="N", default_value=DEFAULT_BUILD_JOBS.as_str())]
+    #[clap(short, long, value_name="N", default_value=DEFAULT_BUILD_JOBS.as_str())]
     pub jobs: usize,
 
     /// Skip compiler version check (not recommended)
-    #[arg(long)]
+    #[clap(long)]
     pub skip_compiler_check: bool,
 
     /// Skip post-build cleanup
-    #[arg(long)]
+    #[clap(long)]
     pub skip_cleanup: bool,
 
     /// Provide more detailed info
-    #[arg(short, long)]
+    #[clap(short, long)]
     pub verbose: bool,
 
     /// Patch file(s)
-    #[arg(required = true)]
+    #[clap(required = true)]
     pub patches: Vec<PathBuf>,
 }
 
