10b70d3e8df6ca3fce41606c93ad2fecc2a3e333
renoseven <dev@renoseven.net>
2023-08-18 15:45:33 +0800

    daemon: initial commit - fast reboot
    Signed-off-by: renoseven <dev@renoseven.net>

---
+++ daemon/src/fast_reboot/kexec.rs
@@ -0,0 +1,58 @@
+use std::ffi::OsString;
+use std::path::Path;
+
+use anyhow::{ensure, Result};
+
+use syscare_common::util::{
+    ext_cmd::{ExternCommand, ExternCommandArgs},
+    os_str::OsStringExt,
+};
+
+const KEXEC: ExternCommand = ExternCommand::new("kexec");
+const SYSTEMCTL: ExternCommand = ExternCommand::new("systemctl");
+
+pub fn load<P, Q>(kernel: P, initramfs: Q) -> Result<()>
+where
+    P: AsRef<Path>,
+    Q: AsRef<Path>,
+{
+    let exit_status = KEXEC.execvp(
+        ExternCommandArgs::new()
+            .arg("--load")
+            .arg(kernel.as_ref())
+            .arg(OsString::from("--initrd=").concat(initramfs.as_ref()))
+            .arg("--reuse-cmdline"),
+    )?;
+    ensure!(
+        exit_status.exit_code() == 0,
+        format!("{}", exit_status.stderr().to_string_lossy())
+    );
+    Ok(())
+}
+
+pub fn unload() -> Result<()> {
+    let exit_status = KEXEC.execvp(ExternCommandArgs::new().arg("--unload"))?;
+    ensure!(
+        exit_status.exit_code() == 0,
+        format!("{}", exit_status.stderr().to_string_lossy())
+    );
+    Ok(())
+}
+
+pub fn systemd_exec() -> Result<()> {
+    let exit_status = SYSTEMCTL.execvp(ExternCommandArgs::new().arg("kexec"))?;
+    ensure!(
+        exit_status.exit_code() == 0,
+        format!("{}", exit_status.stderr().to_string_lossy())
+    );
+    Ok(())
+}
+
+pub fn force_exec() -> Result<()> {
+    let exit_status = KEXEC.execvp(ExternCommandArgs::new().arg("--exec"))?;
+    ensure!(
+        exit_status.exit_code() == 0,
+        format!("{}", exit_status.stderr().to_string_lossy())
+    );
+    Ok(())
+}
---
+++ daemon/src/fast_reboot/skeleton.rs
@@ -0,0 +1,7 @@
+use crate::rpc::{rpc, RpcResult};
+
+#[rpc(server)]
+pub trait FastRebootSkeleton {
+    #[rpc(name = "fast_reboot")]
+    fn fast_reboot(&self, kernel_version: Option<String>, force: bool) -> RpcResult<()>;
+}
