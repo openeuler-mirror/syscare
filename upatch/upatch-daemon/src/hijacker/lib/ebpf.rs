dc4ecd43fcb9562faab5b5f80b172d7135f84fec
renoseven <dev@renoseven.net>
2023-08-29 11:42:39 +0800

    syscare: update dependency guard
    Signed-off-by: renoseven <dev@renoseven.net>

--- upatch/upatch-daemon/src/hijacker/lib/ebpf.rs
+++ upatch/upatch-daemon/src/hijacker/lib/ebpf.rs
@@ -1,64 +1,89 @@
 use std::{
-    path::{Path, PathBuf},
-    process::{Child, Command},
+    path::Path,
+    process::{Child, Command, Stdio},
+    time::Duration,
 };
 
-use anyhow::{Context, Result};
-use log::{debug, error};
+use anyhow::{bail, Context, Result};
+use log::{error, info};
 
 const EBPF_BIN_PATH: &str = "/usr/libexec/syscare/upatch_hijacker";
 const EBPF_SOCKET_PATH: &str = "/var/run/upatch-hijacker";
+const EBPF_WAIT_TIMEOUT: u64 = 100;
+const EBPF_WAIT_MAX_RETRY: u64 = 2;
 
-/// An RAII guard of the hijack ebpf implementation.
+/// An RAII guard of the `upatch_hijack` eBPF program.
 pub struct EbpfProgramGuard {
-    elf_path: PathBuf,
     process: Option<Child>,
 }
 
 impl EbpfProgramGuard {
     pub fn new() -> Result<Self> {
-        let mut instance = Self {
-            elf_path: PathBuf::from(EBPF_BIN_PATH),
-            process: None,
-        };
-        instance.start().context("Failed to start hijacker ebpf")?;
+        let mut instance = Self { process: None };
+        if !Self::exists() {
+            info!("Starting eBPF program...");
+            instance.start().context("Failed to start eBPF program")?;
+        }
 
         Ok(instance)
     }
 }
 
 impl EbpfProgramGuard {
-    fn exists(&self) -> bool {
+    #[inline]
+    fn exists() -> bool {
         Path::new(EBPF_SOCKET_PATH).exists()
     }
 
     fn start(&mut self) -> Result<()> {
-        if !self.exists() {
-            debug!("Starting ebpf program \"{}\"...", EBPF_BIN_PATH);
-            let process = &mut self.process;
-            if process.is_none() {
-                let child = Command::new(&self.elf_path).spawn()?;
-                let _ = process.insert(child);
+        let process = &mut self.process;
+        if process.is_none() {
+            let mut child = Command::new(EBPF_BIN_PATH)
+                .stdout(Stdio::null())
+                .stderr(Stdio::piped())
+                .spawn()?;
+
+            let mut wait_retry = 0;
+            loop {
+                let wait_result = child.try_wait().context("Failed to wait eBPF program")?;
+                match wait_result {
+                    Some(exit_status) => {
+                        if exit_status.code().unwrap_or_default() != 0 {
+                            bail!("Hijacker eBPF program exited unexpectedly");
+                        }
+                    }
+                    None => {
+                        if wait_retry >= EBPF_WAIT_MAX_RETRY {
+                            break;
+                        }
+                        std::thread::sleep(Duration::from_millis(EBPF_WAIT_TIMEOUT));
+                        wait_retry += 1;
+                    }
+                }
             }
+
+            let _ = process.insert(child);
         }
+
         Ok(())
     }
 
     fn stop(&mut self) -> Result<()> {
-        if self.exists() {
-            debug!("Stopping ebpf program \"{}\"...", EBPF_BIN_PATH);
-            if let Some(mut child) = self.process.take() {
-                child.kill()?
-            }
+        if let Some(mut child) = self.process.take() {
+            child.kill()?
         }
+
         Ok(())
     }
 }
 
 impl Drop for EbpfProgramGuard {
     fn drop(&mut self) {
-        if let Err(e) = self.stop().context("Failed to stop hijacker ebpf") {
-            error!("{:?}", e)
+        if Self::exists() {
+            info!("Stopping eBPF program...");
+            if let Err(e) = self.stop().context("Failed to stop eBPF program") {
+                error!("{:?}", e)
+            }
         }
     }
 }
--- upatch/upatch-daemon/src/hijacker/lib/kmod.rs
+++ upatch/upatch-daemon/src/hijacker/lib/kmod.rs
@@ -1,15 +1,15 @@
 use std::{
     ffi::OsStr,
     os::unix::prelude::OsStrExt,
-    path::{Path, PathBuf},
+    path::Path,
     process::{Command, Stdio},
 };
 
 use anyhow::{bail, Context, Result};
-use log::{debug, error};
+use log::{error, info};
 
 const KMOD_NAME: &str = "upatch_hijacker";
-const KMOD_SYS_DIR: &str = "/sys/module";
+const KMOD_SYS_PATH: &str = "/sys/module/upatch_hijacker";
 const CMD_MODPROBE: &str = "modprobe";
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -18,30 +18,31 @@ enum ModuleOperation {
     Remove,
 }
 
-/// An RAII guard of the hijack kernel module implementation.
-pub struct KernelModuleGuard {
-    name: String,
-    sys_path: PathBuf,
-}
+/// An RAII guard of the `upatch_hijacker` kernel module.
+pub struct KernelModuleGuard;
 
 impl KernelModuleGuard {
     pub fn new() -> Result<Self> {
-        let instance: KernelModuleGuard = Self {
-            name: KMOD_NAME.to_string(),
-            sys_path: Path::new(KMOD_SYS_DIR).join(KMOD_NAME),
-        };
-        instance
-            .load()
-            .with_context(|| format!("Failed to load module \"{}\"", instance))?;
+        if !Self::exists() {
+            info!("Loading kernel module...");
+            Self::load().context("Failed to load kernel module")?;
+        }
 
-        Ok(instance)
+        Ok(Self)
     }
 }
 
 impl KernelModuleGuard {
-    fn exec_module_ops(&self, module_op: ModuleOperation) -> Result<()> {
+    #[inline]
+    fn exists() -> bool {
+        Path::new(KMOD_SYS_PATH).exists()
+    }
+
+    fn exec_module_ops(module_op: ModuleOperation) -> Result<()> {
         let mut cmd = Command::new(CMD_MODPROBE);
-        cmd.arg(&self.name).stderr(Stdio::piped());
+        cmd.arg(KMOD_NAME)
+            .stdout(Stdio::null())
+            .stderr(Stdio::piped());
 
         if module_op == ModuleOperation::Remove {
             cmd.arg("--remove");
@@ -57,41 +58,25 @@ impl KernelModuleGuard {
         Ok(())
     }
 
-    fn exists(&self) -> bool {
-        self.sys_path.exists()
+    #[inline]
+    fn load() -> Result<()> {
+        Self::exec_module_ops(ModuleOperation::Insert)
     }
 
-    fn load(&self) -> Result<()> {
-        if !self.exists() {
-            debug!("Loading kernel module \"{}\"...", self);
-            self.exec_module_ops(ModuleOperation::Insert)?;
-        }
-        Ok(())
-    }
-
-    fn unload(&self) -> Result<()> {
-        if self.exists() {
-            debug!("Unloading kernel module \"{}\"...", self);
-            self.exec_module_ops(ModuleOperation::Remove)?;
-        }
-        Ok(())
+    #[inline]
+    fn unload() -> Result<()> {
+        Self::exec_module_ops(ModuleOperation::Remove)
     }
 }
 
 impl Drop for KernelModuleGuard {
     fn drop(&mut self) {
-        if let Err(e) = self
-            .unload()
-            .with_context(|| format!("Failed to unload module \"{}\"", self))
-        {
-            error!("{:?}", e);
+        if Self::exists() {
+            info!("Unloading kernel module...");
+            if let Err(e) = Self::unload().context("Failed to unload kernel module") {
+                error!("{:?}", e);
+            }
         }
-    }
-}
-
-impl std::fmt::Display for KernelModuleGuard {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str(&self.name)
     }
 }
 
