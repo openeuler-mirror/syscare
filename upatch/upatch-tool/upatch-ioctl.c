0c16eef8fe183eaa4c4d6a3557ea3b1504797938
ningyu <ningyu9@huawei.com>
2023-10-16 15:22:46 +0800

    upatch-tool: add active/remove patch ioctl

--- upatch/upatch-tool/upatch-ioctl.c
+++ upatch/upatch-tool/upatch-ioctl.c
@@ -1,24 +1,125 @@
 #include "upatch-ioctl.h"
 
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/ioctl.h>
 
 #include "list.h"
+#include "log.h"
 
-static const char *UPATCH_DEV = "/dev/upatch";
+static const char *UPATCH_DEV = "/dev/upatch-manager";
+
+elf_request_t* build_elf_request(const char *elf_path, const char *patch_path, loff_t offset, pid_t monitor_pid)
+{
+	char buf[PATH_MAX];
+
+	char *e_path = realpath(elf_path, buf);
+	char *p_path = realpath(patch_path, buf);
+
+	elf_request_t *req = calloc(sizeof(elf_request_t), 1);
+	if (!req) {
+		return NULL;
+	}
+
+	memcpy(req->elf_path, e_path, strlen(e_path));
+	memcpy(req->patch_path, p_path, strlen(p_path));
+	req->offset = offset;
+	req->monitor_pid = monitor_pid;
+
+	return req;
+}
 
 int patch_ioctl_apply(const char *target_path, const char *patch_path,
-    struct list_head *symbol_list)
+		struct list_head *symbol_list)
 {
-    // TODO: Call ioctl to request kernel driver to load patch
-    // ioctl -> ko -> register uprobe -> uprobe handler -> execute upatch-manage
-    return 0;
+	// TODO: Call ioctl to request kernel driver to load patch
+	// ioctl -> ko -> register uprobe -> uprobe handler -> execute upatch-manage
+	pid_t monitor_pid = 0;
+	elf_request_t *req = NULL;
+	int ret = -1;
+	patch_symbols_t *sym;
+	int upatch_fd = open(UPATCH_DEV, O_RDWR);
+
+	if (upatch_fd < 0) {
+		log_warn("upatch-ioctl: open dev failed\n");
+		goto out;
+	}
+
+	ret = ioctl(upatch_fd, UPATCH_REGISTER_MONITOR, NULL);
+	if (ret < 0) {
+		log_warn("upatch-ioctl: register monitor ioctl failed\n");
+		goto err;
+	}
+
+	list_for_each_entry(sym, symbol_list, self) {
+		// register_elf
+		req = build_elf_request(target_path, patch_path, sym->offset, monitor_pid);
+		if (!req) {
+			log_warn("upatch-ioctl:build request failed\n");
+			goto err;
+		}
+
+		ret = ioctl(upatch_fd, UPATCH_REGISTER_ELF, req);
+		if (ret < 0) {
+			free(req);
+			log_warn("upatch-ioctl: register elf ioctl failed\n");
+			goto err;
+		}
+		free(req);
+		req = NULL;
+	}
+err:
+	close(upatch_fd);
+out:
+	return ret;
 }
 
 int patch_ioctl_remove(const char *target_path, const char *patch_path,
-    struct list_head *symbol_list)
+		struct list_head *symbol_list)
 {
-    // TODO: Call ioctl to request kernel driver to remove patch
-    // ioctl -> ko -> remove uprobe -> execute upatch-manage
-    return 0;
+	// TODO: Call ioctl to request kernel driver to remove patch
+	// ioctl -> ko -> remove uprobe -> execute upatch-manage
+	pid_t monitor_pid = 0;
+	elf_request_t *req = NULL;
+	int ret = -1;
+	patch_symbols_t *sym;
+	int upatch_fd = open(UPATCH_DEV, O_RDWR);
+
+	if (upatch_fd < 0) {
+		log_warn("upatch-ioctl: open dev failed\n");
+		goto out;
+	}
+
+	monitor_pid = getpid();
+	ret = ioctl(upatch_fd, UPATCH_DEREGISTER_MONITOR, &monitor_pid);
+	if (ret < 0) {
+		log_warn("upatch-ioctl: register monitor ioctl failed\n");
+		goto err;
+	}
+
+	list_for_each_entry(sym, symbol_list, self) {
+		// register_elf
+		req = build_elf_request(target_path, patch_path, sym->offset, monitor_pid);
+		if (!req) {
+			log_warn("upatch-ioctl:build request failed\n");
+			goto err;
+		}
+
+		ret = ioctl(upatch_fd, UPATCH_DEREGISTER_ELF, req);
+		if (ret < 0) {
+			free(req);
+			log_warn("upatch-ioctl: deregister elf ioctl failed\n");
+			goto err;
+		}
+		free(req);
+		req = NULL;
+	}
+err:
+	close(upatch_fd);
+out:
+	return ret;
+	return 0;
 }
--- upatch/upatch-tool/upatch-ioctl.h
+++ upatch/upatch-tool/upatch-ioctl.h
@@ -3,6 +3,32 @@
 
 #include "upatch-meta.h"
 
+#define UPATCH_IOCTL_MAGIC 0xE5
+
+typedef struct elf_request {
+    char elf_path[PATH_MAX];
+    loff_t offset;
+    pid_t monitor_pid;
+    char patch_path[PATH_MAX];
+} elf_request_t;
+
+struct upatch_pid {
+    pid_t *buf;
+    int size;
+};
+
+#define UPATCH_UPROBE_INIT		_IO(UPATCH_IOCTL_MAGIC, 0x5)
+#define UPATCH_UPROBE_DESTROY		_IO(UPATCH_IOCTL_MAGIC, 0x6)
+#define UPATCH_REGISTER_ELF		_IOW(UPATCH_IOCTL_MAGIC, 0x7, const elf_request_t *)
+#define UPATCH_DEREGISTER_ELF		_IOW(UPATCH_IOCTL_MAGIC, 0x8, const elf_request_t *)
+#define UPATCH_GET_PID			_IOW(UPATCH_IOCTL_MAGIC, 0x9, struct upatch_pid *)
+#define UPATCH_REGISTER_MONITOR		_IO(UPATCH_IOCTL_MAGIC, 0x10)
+#define UPATCH_DEREGISTER_MONITOR	_IO(UPATCH_IOCTL_MAGIC, 0x11)
+
+
+
+long handle_ioctl(struct file *filp, unsigned int cmd, unsigned long arg);
+
 int patch_ioctl_apply(const char *target_path, const char *patch_path,
     struct list_head *symbol_list);
 
