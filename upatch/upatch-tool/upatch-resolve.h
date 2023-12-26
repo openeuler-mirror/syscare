99015d1b5ce010fa95e5e40e92c9ef61f91a32a4
renoseven <dev@renoseven.net>
2023-12-26 13:07:53 +0800

    upatch: fix memory leak
    Signed-off-by: renoseven <dev@renoseven.net>

--- upatch/upatch-tool/upatch-resolve.h
+++ upatch/upatch-tool/upatch-resolve.h
@@ -2,8 +2,9 @@
 #define __UPATCH_RESOLVE_H_
 
 #include "list.h"
+#include "upatch-elf.h"
 
-struct list_head* patch_symbols_resolve(const char *target_elf, const char *patch_file);
+struct list_head* patch_symbols_resolve(struct upatch_elf *uelf, struct running_elf *relf);
 void patch_symbols_free(struct list_head *symbols);
 
 #endif
