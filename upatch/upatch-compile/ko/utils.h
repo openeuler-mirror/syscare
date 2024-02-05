a5aae9bbb8d9c90363c97b73a74bdefa2b4267a1
renoseven <dev@renoseven.net>
2024-02-05 16:12:53 +0800

    upatch: uprobe based hijacker - kmod
    Switch upatch-hijacker under layer mechanism from
    from kprobe to uprobe, since the entrance function
    'execve' & it's inner implementation in kernel may
    be inlined by the compiler, which is not stable as
    libc's implementation as a hook entrance.
    
    The basic mechanism of the hijacker is not changed.
    
    Signed-off-by: renoseven <dev@renoseven.net>

---
+++ upatch/upatch-compile/ko/utils.h
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Authors:
+ *   RenoSeven <dev@renoseven.net>
+ */
+
+#ifndef _UPATCH_HIJACKER_KO_UTILS_H
+#define _UPATCH_HIJACKER_KO_UTILS_H
+
+#include <stdbool.h>
+
+#include <linux/fs.h>
+#include <linux/namei.h>
+#include <linux/path.h>
+#include <linux/pid_namespace.h>
+
+static inline struct inode* path_inode(const char *path)
+{
+    struct path kpath;
+
+    if (kern_path(path, LOOKUP_NO_SYMLINKS, &kpath) != 0) {
+        return NULL;
+    }
+    return kpath.dentry->d_inode;
+}
+
+static inline bool inode_equal(const struct inode *lhs, const struct inode *rhs)
+{
+    return (lhs->i_ino == rhs->i_ino);
+}
+
+static inline bool ns_equal(const struct pid_namespace *lhs,
+    const struct pid_namespace *rhs)
+{
+    return (lhs->ns.inum == rhs->ns.inum);
+}
+
+#endif /* _UPATCH_HIJACKER_KO_UTILS_H */
