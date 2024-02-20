fb0469a00fcbd9bfcb7749cbad4128a89e6cdb24
renoseven <dev@renoseven.net>
2024-02-20 12:35:05 +0800

    remove useless files
    1. remove lib directory
    2. remove misc/patch directory
    3. remove daemon/src/event directory
    4. remove builder/src/package/dependency.rs
    
    Signed-off-by: renoseven <dev@renoseven.net>

--- daemon/src/event/manager.rs
+++
@@ -1,9 +0,0 @@
-use std::sync::Arc;
-
-use indexmap::IndexMap;
-
-use super::trx::{TxChannel, RxChannel};
-
-pub struct EventManager {
-    // topic_map: IndexMap<String, (Arc<TxChannel>, Arc<RxChannel>)>
-}
