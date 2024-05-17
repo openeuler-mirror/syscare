cf5217d55b2a603cfa0a852d876809c536835f18
renoseven <dev@renoseven.net>
2024-05-17 14:46:30 +0800

    syscared: fix 'apply kernel module patch failure' issue
    Signed-off-by: renoseven <dev@renoseven.net>

--- syscared/src/patch/driver/kpatch/mod.rs
+++ syscared/src/patch/driver/kpatch/mod.rs
@@ -128,6 +128,9 @@ impl KernelPatchDriver {
         debug!("Patch target:   '{}'", patch_target);
         debug!("Current kernel: '{}'", current_kernel.to_string_lossy());
 
+        if !patch_target.starts_with("KERNEL_NAME_PREFIX") {
+            return Ok(());
+        }
         ensure!(
             current_kernel == patch_target,
             "Kpatch: Patch is incompatible",
