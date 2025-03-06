06a20921cfd97c7916c3fe2165d031a33093a652
renoseven <dev@renoseven.net>
2025-03-06 16:48:10 +0800

    syscare: rewrite syscare cli
    1. remove 'build' subcommand
    2. remove Executor trait, execute subcommand directly
    3. process unknown subcommand as external command
    4. move rpc logic into RpcClient
    5. move patch logic into PatchProxy
    6. rename 'patch_op.lock' to 'syscare.lock'
    7. change umask from 077 to 022
    
    Signed-off-by: renoseven <dev@renoseven.net>

---
+++ syscare/src/rpc/client.rs
@@ -0,0 +1,144 @@
+// SPDX-License-Identifier: Mulan PSL v2
+/*
+ * Copyright (c) 2024 Huawei Technologies Co., Ltd.
+ * syscare is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *         http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
+use std::{
+    ops::Deref,
+    path::{Path, PathBuf},
+};
+
+use anyhow::{anyhow, ensure, Error, Result};
+use jsonrpc::{
+    serde_json::value::RawValue,
+    simple_uds::{self, UdsTransport},
+    Client,
+};
+use log::debug;
+use serde::{Deserialize, Serialize};
+
+use syscare_common::fs::{FileLock, FileLockType};
+
+#[derive(Debug, Default)]
+pub struct RpcArguments {
+    args: Vec<Box<RawValue>>,
+}
+
+impl RpcArguments {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn arg<T: Serialize>(mut self, arg: T) -> Self {
+        self.args.push(jsonrpc::arg(arg));
+        self
+    }
+}
+
+impl Deref for RpcArguments {
+    type Target = [Box<RawValue>];
+
+    fn deref(&self) -> &Self::Target {
+        &self.args
+    }
+}
+
+pub struct RpcClient {
+    lock: PathBuf,
+    socket: PathBuf,
+    client: Client,
+}
+
+impl RpcClient {
+    pub fn new(work_dir: &Path) -> Result<Self> {
+        const LOCK_FILE_NAME: &str = "syscare.lock";
+        const SOCKET_FILE_NAME: &str = "syscared.sock";
+
+        ensure!(
+            work_dir.is_dir(),
+            "Working directory '{}' is invalid",
+            work_dir.display()
+        );
+        let lock = work_dir.join(LOCK_FILE_NAME);
+        let socket = work_dir.join(SOCKET_FILE_NAME);
+        let client = Client::with_transport(UdsTransport::new(&socket));
+
+        Ok(Self {
+            lock,
+            socket,
+            client,
+        })
+    }
+
+    pub fn lock(&self) -> Result<FileLock> {
+        FileLock::new(&self.lock, FileLockType::Exclusive)
+    }
+
+    pub fn call<T>(&self, cmd: &str) -> Result<T>
+    where
+        T: for<'a> Deserialize<'a>,
+    {
+        self.call_with_args::<T>(cmd, RpcArguments::default())
+    }
+
+    pub fn call_with_args<T>(&self, cmd: &str, args: RpcArguments) -> Result<T>
+    where
+        T: for<'a> Deserialize<'a>,
+    {
+        let request = self.client.build_request(cmd, &args);
+        debug!("{:#?}", request);
+
+        let response = self
+            .client
+            .send_request(request)
+            .map_err(|e| self.parse_request_error(e))?;
+        debug!("{:#?}", response);
+
+        response.result().map_err(|e| self.parse_response_error(e))
+    }
+}
+
+impl RpcClient {
+    fn parse_transport_error(&self, error: Box<dyn std::error::Error + Send + Sync>) -> Error {
+        anyhow!(
+            "Cannot connect to syscare daemon at unix://{}, {}",
+            self.socket.display(),
+            match error.downcast::<simple_uds::Error>() {
+                Ok(err) => match *err {
+                    simple_uds::Error::SocketError(e) => e.to_string(),
+                    simple_uds::Error::Timeout => String::from("Connection timeout"),
+                    simple_uds::Error::Json(_) => String::from("Invalid data format"),
+                },
+                Err(_) => {
+                    String::from("Unknown error")
+                }
+            }
+        )
+    }
+
+    fn parse_request_error(&self, error: jsonrpc::Error) -> Error {
+        match error {
+            jsonrpc::Error::Transport(err) => self.parse_transport_error(err),
+            jsonrpc::Error::Json(_) => anyhow!("Request format error"),
+            _ => anyhow!("Unknown request error"),
+        }
+    }
+
+    fn parse_response_error(&self, error: jsonrpc::Error) -> Error {
+        match error {
+            jsonrpc::Error::Transport(err) => self.parse_transport_error(err),
+            jsonrpc::Error::Json(_) => anyhow!("Response format error"),
+            jsonrpc::Error::Rpc(_) => anyhow!("Request is unrecognized"),
+            _ => anyhow!("Unknown response error"),
+        }
+    }
+}
--- syscare/src/rpc/proxy.rs
+++ syscare/src/rpc/proxy.rs
@@ -12,100 +12,329 @@
  * See the Mulan PSL v2 for more details.
  */
 
-use std::rc::Rc;
+use std::fmt::Write;
 
-use anyhow::Result;
+use anyhow::{anyhow, Error, Result};
 use function_name::named;
 
+use log::info;
 use syscare_abi::{PackageInfo, PatchInfo, PatchListRecord, PatchStateRecord};
 
-use super::{args::RpcArguments, remote::RpcRemote};
+use super::client::{RpcArguments, RpcClient};
 
-pub struct RpcProxy {
-    remote: Rc<RpcRemote>,
+pub struct PatchProxy<'a> {
+    client: &'a RpcClient,
 }
 
-impl RpcProxy {
-    pub fn new(remote: Rc<RpcRemote>) -> Self {
-        Self { remote }
+impl<'a> PatchProxy<'a> {
+    pub fn new(client: &'a RpcClient) -> Self {
+        Self { client }
     }
+}
 
+/* RPC methods */
+impl PatchProxy<'_> {
     #[named]
-    pub fn check_patch(&self, identifier: &str) -> Result<()> {
-        self.remote
+    fn check_patch(&self, identifier: &str) -> Result<()> {
+        self.client
             .call_with_args(function_name!(), RpcArguments::new().arg(identifier))
     }
 
     #[named]
-    pub fn apply_patch(&self, identifier: &str, force: bool) -> Result<Vec<PatchStateRecord>> {
-        self.remote.call_with_args(
+    fn apply_patch(&self, identifier: &str, force: bool) -> Result<Vec<PatchStateRecord>> {
+        self.client.call_with_args(
             function_name!(),
             RpcArguments::new().arg(identifier).arg(force),
         )
     }
 
     #[named]
-    pub fn remove_patch(&self, identifier: &str) -> Result<Vec<PatchStateRecord>> {
-        self.remote
+    fn remove_patch(&self, identifier: &str) -> Result<Vec<PatchStateRecord>> {
+        self.client
             .call_with_args(function_name!(), RpcArguments::new().arg(identifier))
     }
 
     #[named]
-    pub fn active_patch(&self, identifier: &str, force: bool) -> Result<Vec<PatchStateRecord>> {
-        self.remote.call_with_args(
+    fn active_patch(&self, identifier: &str, force: bool) -> Result<Vec<PatchStateRecord>> {
+        self.client.call_with_args(
             function_name!(),
             RpcArguments::new().arg(identifier).arg(force),
         )
     }
 
     #[named]
-    pub fn deactive_patch(&self, identifier: &str) -> Result<Vec<PatchStateRecord>> {
-        self.remote
+    fn deactive_patch(&self, identifier: &str) -> Result<Vec<PatchStateRecord>> {
+        self.client
             .call_with_args(function_name!(), RpcArguments::new().arg(identifier))
     }
 
     #[named]
-    pub fn accept_patch(&self, identifier: &str) -> Result<Vec<PatchStateRecord>> {
-        self.remote
+    fn accept_patch(&self, identifier: &str) -> Result<Vec<PatchStateRecord>> {
+        self.client
             .call_with_args(function_name!(), RpcArguments::new().arg(identifier))
     }
 
     #[named]
-    pub fn get_patch_list(&self) -> Result<Vec<PatchListRecord>> {
-        self.remote.call(function_name!())
+    fn get_patch_list(&self) -> Result<Vec<PatchListRecord>> {
+        self.client.call(function_name!())
     }
 
     #[named]
-    pub fn get_patch_status(&self, identifier: &str) -> Result<Vec<PatchStateRecord>> {
-        self.remote
+    fn get_patch_status(&self, identifier: &str) -> Result<Vec<PatchStateRecord>> {
+        self.client
             .call_with_args(function_name!(), RpcArguments::new().arg(identifier))
     }
 
     #[named]
-    pub fn get_patch_info(&self, identifier: &str) -> Result<PatchInfo> {
-        self.remote
+    fn get_patch_info(&self, identifier: &str) -> Result<PatchInfo> {
+        self.client
             .call_with_args(function_name!(), RpcArguments::new().arg(identifier))
     }
 
     #[named]
-    pub fn get_patch_target(&self, identifier: &str) -> Result<PackageInfo> {
-        self.remote
+    fn get_patch_target(&self, identifier: &str) -> Result<PackageInfo> {
+        self.client
             .call_with_args(function_name!(), RpcArguments::new().arg(identifier))
     }
 
     #[named]
-    pub fn save_patch_status(&self) -> Result<()> {
-        self.remote.call(function_name!())
+    fn save_patch_status(&self) -> Result<()> {
+        self.client.call(function_name!())
     }
 
     #[named]
-    pub fn restore_patch_status(&self, accepted_only: bool) -> Result<()> {
-        self.remote
+    fn restore_patch_status(&self, accepted_only: bool) -> Result<()> {
+        self.client
             .call_with_args(function_name!(), RpcArguments::new().arg(accepted_only))
     }
 
     #[named]
-    pub fn rescan_patches(&self) -> Result<()> {
-        self.remote.call(function_name!())
+    fn rescan_patches(&self) -> Result<()> {
+        self.client.call(function_name!())
+    }
+}
+
+/* Internal methods */
+impl PatchProxy<'_> {
+    fn check_error(mut errors: Vec<Error>) -> Result<()> {
+        match errors.len() {
+            0 => Ok(()),
+            1 => Err(errors.remove(0)),
+            _ => {
+                let mut err_msg = String::new();
+                for (idx, e) in errors.into_iter().enumerate() {
+                    writeln!(err_msg, "{}. {}", idx, e)?;
+                }
+                err_msg.pop();
+
+                Err(anyhow!(err_msg))
+            }
+        }
+    }
+}
+
+/* External methods */
+impl PatchProxy<'_> {
+    pub fn show_patch_info(&self, identifiers: &[String]) -> Result<()> {
+        let _ = self.client.lock()?;
+
+        let mut results = vec![];
+        let mut errors = vec![];
+
+        for identifier in identifiers {
+            match self.get_patch_info(identifier) {
+                Ok(patch_info) => results.push((identifier.as_str(), patch_info)),
+                Err(e) => errors.push(e),
+            }
+        }
+
+        let mut result_iter = results.into_iter().peekable();
+        while let Some((identifier, patch_info)) = result_iter.next() {
+            info!("-------------------------------------------");
+            info!("Patch: {}", identifier);
+            info!("-------------------------------------------");
+            info!("{}", patch_info);
+            if result_iter.peek().is_some() {
+                continue;
+            }
+            info!("-------------------------------------------");
+        }
+        Self::check_error(errors)
+    }
+
+    pub fn show_patch_target(&self, identifiers: &[String]) -> Result<()> {
+        let _ = self.client.lock()?;
+
+        let mut results = vec![];
+        let mut errors = vec![];
+
+        for identifier in identifiers {
+            match self.get_patch_target(identifier) {
+                Ok(pkg_info) => results.push((identifier.as_str(), pkg_info)),
+                Err(e) => errors.push(e),
+            }
+        }
+
+        let mut result_iter = results.into_iter().peekable();
+        while let Some((identifier, pkg_info)) = result_iter.next() {
+            info!("-------------------------------------------");
+            info!("Patch: {}", identifier);
+            info!("-------------------------------------------");
+            info!("{}", pkg_info);
+            if result_iter.peek().is_some() {
+                continue;
+            }
+            info!("-------------------------------------------");
+        }
+        Self::check_error(errors)
+    }
+
+    pub fn show_patch_status(&self, identifiers: &[String]) -> Result<()> {
+        let _ = self.client.lock()?;
+
+        let mut results = vec![];
+        let mut errors = vec![];
+
+        for identifier in identifiers {
+            match self.get_patch_status(identifier) {
+                Ok(status_list) => results.extend(status_list),
+                Err(e) => errors.push(e),
+            }
+        }
+
+        for record in results {
+            info!("{}: {}", record.name, record.status)
+        }
+        Self::check_error(errors)
+    }
+
+    pub fn show_patch_list(&self) -> Result<()> {
+        let _ = self.client.lock()?;
+
+        let patch_list = self.get_patch_list()?;
+
+        info!("{:<40} {:<60} {:<12}", "Uuid", "Name", "Status");
+        for patch in patch_list {
+            info!("{:<40} {:<60} {:<12}", patch.uuid, patch.name, patch.status)
+        }
+
+        Ok(())
+    }
+
+    pub fn check_patches(&self, identifiers: &[String]) -> Result<()> {
+        let _ = self.client.lock()?;
+
+        let mut errors = vec![];
+        for identifier in identifiers {
+            if let Err(e) = self.check_patch(identifier) {
+                errors.push(e);
+            }
+        }
+        Self::check_error(errors)
+    }
+
+    pub fn apply_patches(&self, identifiers: &[String], force: bool) -> Result<()> {
+        let _ = self.client.lock()?;
+
+        let mut results = vec![];
+        let mut errors = vec![];
+        for identifier in identifiers {
+            match self.apply_patch(identifier, force) {
+                Ok(status_list) => results.extend(status_list),
+                Err(e) => errors.push(e),
+            }
+        }
+
+        for result in results {
+            info!("{}: {}", result.name, result.status);
+        }
+        Self::check_error(errors)
+    }
+
+    pub fn remove_patches(&self, identifiers: &[String]) -> Result<()> {
+        let _ = self.client.lock()?;
+
+        let mut results = vec![];
+        let mut errors = vec![];
+        for identifier in identifiers {
+            match self.remove_patch(identifier) {
+                Ok(status_list) => results.extend(status_list),
+                Err(e) => errors.push(e),
+            }
+        }
+
+        for result in results {
+            info!("{}: {}", result.name, result.status);
+        }
+        Self::check_error(errors)
+    }
+
+    pub fn active_patches(&self, identifiers: &[String], force: bool) -> Result<()> {
+        let _ = self.client.lock()?;
+
+        let mut results = vec![];
+        let mut errors = vec![];
+        for identifier in identifiers {
+            match self.active_patch(identifier, force) {
+                Ok(status_list) => results.extend(status_list),
+                Err(e) => errors.push(e),
+            }
+        }
+
+        for result in results {
+            info!("{}: {}", result.name, result.status);
+        }
+        Self::check_error(errors)
+    }
+
+    pub fn deactive_patches(&self, identifiers: &[String]) -> Result<()> {
+        let _ = self.client.lock()?;
+
+        let mut results = vec![];
+        let mut errors = vec![];
+        for identifier in identifiers {
+            match self.deactive_patch(identifier) {
+                Ok(status_list) => results.extend(status_list),
+                Err(e) => errors.push(e),
+            }
+        }
+
+        for result in results {
+            info!("{}: {}", result.name, result.status);
+        }
+        Self::check_error(errors)
+    }
+
+    pub fn accept_patches(&self, identifiers: &[String]) -> Result<()> {
+        let _ = self.client.lock()?;
+
+        let mut results = vec![];
+        let mut errors = vec![];
+        for identifier in identifiers {
+            match self.accept_patch(identifier) {
+                Ok(status_list) => results.extend(status_list),
+                Err(e) => errors.push(e),
+            }
+        }
+
+        for result in results {
+            info!("{}: {}", result.name, result.status);
+        }
+        Self::check_error(errors)
+    }
+
+    pub fn save_patches(&self) -> Result<()> {
+        let _ = self.client.lock()?;
+        self.save_patch_status()
+    }
+
+    pub fn restore_patches(&self, accepted_only: bool) -> Result<()> {
+        let _ = self.client.lock()?;
+        self.restore_patch_status(accepted_only)
+    }
+
+    pub fn rescan_all_patches(&self) -> Result<()> {
+        let _ = self.client.lock()?;
+        self.rescan_patches()
     }
 }
