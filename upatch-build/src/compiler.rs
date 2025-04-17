84b924bc883fa1f3590ef08689a297f9b5ad0d29
renoseven <dev@renoseven.net>
2025-04-17 09:49:22 +0800

    upatch-build: rewrite compiler detection
    Dwarf:
    1. remove dwarf::{Dwarf, Relocation}
    2. add dwarf::{Producer, ProducerType, ProducerParser, ProducerIterator}
    3. rewrite pruducer name parsing logic
    4. rewrite producer vesrion parsing logic
    5. support llvm assembler & clang compiler
    
    Compiler:
    1. rename compiler::CompilerInfo -> compiler::Compiler
    2. detection only compiles one test object
    3. support cross compiler
    4. support llvm linker
    
    Builder:
    1. rewrite compiler detection logic
    2. rewrite compiler version check logic
    3. rewrite patch linking logic
    
    Signed-off-by: renoseven <dev@renoseven.net>

--- upatch-build/src/compiler.rs
+++ upatch-build/src/compiler.rs
@@ -13,183 +13,171 @@
  */
 
 use std::{
-    ffi::OsString,
+    ffi::{OsStr, OsString},
+    os::unix::ffi::OsStrExt,
     path::{Path, PathBuf},
 };
 
-use anyhow::{Context, Result};
-use indexmap::{IndexMap, IndexSet};
-use log::debug;
+use anyhow::{bail, Context, Result};
+use indexmap::IndexSet;
 use which::which;
 
-use syscare_common::{ffi::OsStrExt, fs, process::Command};
+use syscare_common::{concat_os, ffi::OsStrExt as _, fs, process::Command};
 
-use crate::dwarf::{Dwarf, ProducerType};
-
-const STD_NAMES: [&str; 45] = [
-    "c89", "c90", "c99", "c9x", "c11", "c17", "c18", "c1x", "c2x", "gnu", "gnu89", "gnu90",
-    "gnu99", "gnu9x", "gnu11", "gnu17", "gnu18", "gnu1x", "gnu2x", "c++98", "c++03", "c++0x",
-    "c++11", "c++14", "c++17", "c++1y", "c++1z", "c++20", "c++2a", "gnu++98", "gnu++03", "gnu++0x",
-    "gnu++11", "gnu++14", "gnu++17", "gnu++1y", "gnu++1z", "gnu++20", "gnu++2a", "f95", "f2003",
-    "f2008", "f2008ts", "f2018", "legacy",
-];
-const ASSEMBLER_NAME: &str = "as";
-const LINKER_NAME: &str = "ld";
+use crate::dwarf::{Producer, ProducerParser, ProducerType};
 
 #[derive(Debug, Clone)]
-pub struct CompilerInfo {
-    pub binary: PathBuf,
-    pub assembler: PathBuf,
+pub struct Compiler {
+    pub prefix: Option<OsString>,
+    pub name: OsString,
+    pub kind: ProducerType,
+    pub version: OsString,
+    pub path: PathBuf,
     pub linker: PathBuf,
-    pub producers: IndexSet<OsString>,
 }
 
-impl CompilerInfo {
-    fn get_component_name<P: AsRef<Path>>(compiler: P, name: &str) -> Result<OsString> {
-        let output = Command::new(compiler.as_ref())
-            .arg(format!("-print-prog-name={}", name))
-            .run_with_output()?;
-
-        output.exit_ok()?;
-        Ok(output.stdout)
-    }
-
-    fn build_test_objects<P, Q, R>(
-        binary: P,
-        assembler: Q,
-        output_dir: R,
-    ) -> Result<IndexSet<PathBuf>>
+impl Compiler {
+    pub fn parse<P, Q>(path: P, output_dir: Q) -> Result<Compiler>
     where
         P: AsRef<Path>,
         Q: AsRef<Path>,
-        R: AsRef<Path>,
     {
-        let source_file = output_dir.as_ref().join("test.c");
-        let assembly_file = output_dir.as_ref().join("test.S");
-        let assembly_object = output_dir.as_ref().join("test.o");
+        let path = path.as_ref().to_path_buf();
+        let name = path
+            .file_name()
+            .context("Failed to parse compiler name")?
+            .to_os_string();
 
-        fs::write(&source_file, "int main() { return 0; }")?;
+        let output_path = Self::build_test_object(&path, &name, output_dir.as_ref())
+            .context("Failed to build test object")?;
+        let prefix = Self::parse_compiler_prefix(&name).map(OsStr::to_os_string);
+        let producer = Self::parse_compiler_producer(&output_path)
+            .context("Failed to parse compiler producer")?;
+        let linker = Self::get_compiler_linker(&path, &prefix, &producer)
+            .context("Failed to get compiler linker")?;
 
-        let mut object_files = IndexSet::new();
+        Ok(Self {
+            prefix,
+            name,
+            kind: producer.kind,
+            version: producer.version,
+            path,
+            linker,
+        })
+    }
 
-        Command::new(binary.as_ref())
-            .arg("-S")
+    fn build_test_object(path: &Path, name: &OsStr, output_dir: &Path) -> Result<PathBuf> {
+        let source_file = output_dir.join("test.c");
+        let output_file = output_dir.join(concat_os!(name, "-test"));
+
+        if !source_file.exists() {
+            fs::write(&source_file, "int main() { return 0; }")?;
+        }
+        Command::new(path)
+            .arg("-g")
             .arg(&source_file)
             .arg("-o")
-            .arg(&assembly_file)
+            .arg(&output_file)
             .run()?
             .exit_ok()?;
 
-        Command::new(assembler.as_ref())
-            .arg("-g")
-            .arg(assembly_file)
-            .arg("-o")
-            .arg(&assembly_object)
-            .run()?
-            .exit_ok()?;
-
-        object_files.insert(assembly_object);
-
-        for std_name in STD_NAMES {
-            let compiler_object = output_dir.as_ref().join(format!("test_{}.o", std_name));
-            let build_success = Command::new(binary.as_ref())
-                .arg(format!("-std={}", std_name))
-                .args(["-g", "-c"])
-                .arg(&source_file)
-                .arg("-o")
-                .arg(&compiler_object)
-                .stderr(log::Level::Trace)
-                .run_with_output()?
-                .success();
-
-            if build_success {
-                object_files.insert(compiler_object);
-            }
-        }
-        object_files.sort();
-
-        Ok(object_files)
+        Ok(output_file)
     }
 
-    fn run_compiler_detection<P, Q, R>(
-        binary: P,
-        assembler: Q,
-        output_dir: R,
-    ) -> Result<IndexSet<OsString>>
-    where
-        P: AsRef<Path>,
-        Q: AsRef<Path>,
-        R: AsRef<Path>,
-    {
-        let mut producers = IndexSet::new();
+    fn parse_compiler_prefix(compiler: &OsStr) -> Option<&OsStr> {
+        /*
+         * Matches compiler prefix of compiler name
+         * eg. x86_64-linux-gnu-gcc       -> x86_64-linux-gnu-
+         * eg. aarch64-target-linux-clang -> aarch64-target-linux-
+         */
+        let slice = compiler.as_bytes();
 
-        let test_objects = Self::build_test_objects(binary, assembler, &output_dir)
-            .context("Failed to build test objects")?;
-        for test_object in test_objects {
-            producers.extend(Dwarf::parse(test_object)?.producers());
+        let spliter_indices = slice.iter().enumerate().rev().filter_map(|(index, &b)| {
+            if b == b'-' {
+                return Some(index);
+            }
+            None
+        });
+
+        for pos in spliter_indices {
+            let (prefix, name) = slice.split_at(pos + 1);
+            if name.iter().any(|&b| !b.is_ascii_digit()) {
+                return Some(OsStr::from_bytes(prefix));
+            }
         }
-        producers.sort();
 
-        fs::remove_dir_all(&output_dir)?;
-        Ok(producers)
+        None
+    }
+
+    fn parse_compiler_producer(path: &Path) -> Result<Producer> {
+        let producer_parser = ProducerParser::open(path)
+            .with_context(|| format!("Failed to open {}", path.display()))?;
+        let producer_iter = producer_parser
+            .parse()
+            .with_context(|| format!("Failed to parse {}", path.display()))?;
+
+        let mut producer_map = IndexSet::new();
+        for parse_result in producer_iter {
+            let producer = parse_result.context("Failed to parse object producer")?;
+            producer_map.insert(producer);
+        }
+        producer_map.sort();
+
+        // Compiler producer would be highest supported.
+        producer_map.pop().context("No object producer")
+    }
+
+    fn get_component(
+        path: &Path,
+        prefix: &Option<OsString>,
+        name: &str,
+    ) -> Result<Option<PathBuf>> {
+        let get_component_path = |name: &OsStr| -> Result<Option<PathBuf>> {
+            let output = Command::new(path)
+                .arg(concat_os!("-print-prog-name=", name))
+                .run_with_output()?;
+            output.exit_ok()?;
+            Ok(which(output.stdout.trim()).ok())
+        };
+
+        if let Some(prefixed_name) = prefix.as_ref().map(|pfx| concat_os!(pfx, name)) {
+            let component = get_component_path(&prefixed_name)?;
+            if component.is_some() {
+                return Ok(component);
+            }
+        }
+        get_component_path(OsStr::new(name))
+    }
+
+    fn get_compiler_linker(
+        path: &Path,
+        prefix: &Option<OsString>,
+        producer: &Producer,
+    ) -> Result<PathBuf> {
+        const CLANG_LINKER_NAMES: &[&str] = &["ld.lld", "ld"];
+        const GNU_LINKER_NAMES: &[&str] = &["ld"];
+
+        let linkers = if matches!(producer.kind, ProducerType::ClangC | ProducerType::ClangCxx) {
+            // Clang may use llvm linker, we will try it firstly.
+            CLANG_LINKER_NAMES
+        } else {
+            GNU_LINKER_NAMES
+        };
+        for name in linkers {
+            if let Some(path) = Self::get_component(path, prefix, name)? {
+                return Ok(path);
+            }
+        }
+        bail!("No suitable linker")
     }
 }
 
-impl CompilerInfo {
-    pub fn parse<I, P, Q>(compilers: I, temp_dir: Q) -> Result<IndexMap<ProducerType, CompilerInfo>>
-    where
-        I: IntoIterator<Item = P>,
-        P: AsRef<Path>,
-        Q: AsRef<Path>,
-    {
-        let mut compiler_map = IndexMap::new();
-
-        for compiler in compilers {
-            let binary_file = compiler.as_ref();
-            let binary_name = binary_file
-                .file_name()
-                .context("Failed to parse binary name")?;
-
-            let output_dir = temp_dir.as_ref().join(binary_name);
-            fs::create_dir_all(&output_dir)?;
-
-            debug!("- Checking {}", binary_file.display());
-            let assembler_name = Self::get_component_name(binary_file, ASSEMBLER_NAME)
-                .with_context(|| {
-                    format!("Failed to get assembler name of {}", binary_file.display())
-                })?;
-            let assembler = fs::normalize(which(assembler_name.trim()).with_context(|| {
-                format!("Cannot find assembler {}", assembler_name.to_string_lossy())
-            })?)?;
-
-            let linker_name =
-                Self::get_component_name(binary_file, LINKER_NAME).with_context(|| {
-                    format!("Failed to get linker name of {}", binary_file.display())
-                })?;
-            let linker = fs::normalize(which(linker_name.trim()).with_context(|| {
-                format!("Cannot find linker {}", linker_name.to_string_lossy())
-            })?)?;
-            let producers = Self::run_compiler_detection(&compiler, &assembler, &output_dir)
-                .context("Failed to detect compiler")?;
-
-            for producer in &producers {
-                let producer_type = ProducerType::from(producer);
-                if (producer_type == ProducerType::As) || (producer_type == ProducerType::Unknown) {
-                    continue;
-                }
-                compiler_map.insert(
-                    producer_type,
-                    Self {
-                        binary: binary_file.to_path_buf(),
-                        assembler: assembler.clone(),
-                        linker: linker.clone(),
-                        producers: producers.clone(),
-                    },
-                );
-            }
-        }
-        compiler_map.sort_keys();
-
-        Ok(compiler_map)
+impl std::fmt::Display for Compiler {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!(
+            "{}{}",
+            self.prefix.as_deref().unwrap_or_default().to_string_lossy(),
+            self.name.to_string_lossy()
+        ))
     }
 }
