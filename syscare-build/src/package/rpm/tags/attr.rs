7298cf27dc5a368c51767fb49a9c15fc913c5890
renoseven <dev@renoseven.net>
2025-08-30 15:19:20 +0800

    syscare-build: fix compile failure on rust 1.89
    Signed-off-by: renoseven <dev@renoseven.net>

--- syscare-build/src/package/rpm/tags/attr.rs
+++ syscare-build/src/package/rpm/tags/attr.rs
@@ -29,32 +29,8 @@ impl std::fmt::Display for RpmDefAttr {
     }
 }
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
-pub struct RpmAttr {
-    pub mode: u32,
-    pub user: String,
-    pub group: String,
-}
-
-impl std::fmt::Display for RpmAttr {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_fmt(format_args!(
-            "%attr({:o},{},{})",
-            self.mode, self.user, self.group
-        ))
-    }
-}
-
 #[test]
 fn test() {
-    let attr = RpmAttr {
-        mode: 0o755,
-        user: String::from("root"),
-        group: String::from("nobody"),
-    };
-    println!("RpmAttr::new()\n{}\n", attr);
-    assert_eq!(attr.to_string(), "%attr(755,root,nobody)");
-
     let def_attr = RpmDefAttr {
         file_mode: 0o755,
         user: String::from("root"),
