505ab7057702aff595c527e7ae229f941df6be6e
eshiner <zengfuliangjx@sina.cn>
2026-03-11 16:55:30 +0800

    upatch-build: fix apply patch to source error
    Fix issue where user patch built from source code with --override-line-macros
    
    Signed-off-by: eshiner <zengfuliangjx@sina.cn>

--- upatch-build/src/project.rs
+++ upatch-build/src/project.rs
@@ -186,9 +186,23 @@ impl Project<'_> {
         Ok(())
     }
 
-    pub fn override_line_macros(&self) -> Result<()> {
+    pub fn restore_backup_files(&self, backup_files: &[PathBuf]) -> Result<()> {
+        for file_path in backup_files {
+            let backup_path = PathBuf::from(concat_os!(file_path, ".bak"));
+            if backup_path.exists() {
+                fs::rename(&backup_path, file_path)?;
+                debug!("Restored: {}", backup_path.to_string_lossy());
+            }
+        }
+
+        Ok(())
+    }
+
+    pub fn override_line_macros(&self) -> Result<Vec<PathBuf>> {
         const LINE_MACRO_NAME: &str = "__LINE__";
         const LINE_MACRO_VALUE: &str = "0";
+
+        let mut backup_files = Vec::new();
 
         let file_list = fs::list_files(self.source_dir, fs::TraverseOptions { recursive: true })?;
         for file_path in file_list {
@@ -202,12 +216,17 @@ impl Project<'_> {
                 .replace(LINE_MACRO_NAME, LINE_MACRO_VALUE)
                 .into_vec();
             if old_contents != new_contents {
+                // prepare to backup original file
+                let backup_path = PathBuf::from(concat_os!(&file_path, ".bak"));
+                fs::copy(&file_path, &backup_path)?;
+                backup_files.push(file_path.clone());
+
                 debug!("* {}", file_path.display());
                 fs::write(&file_path, new_contents)?;
             }
         }
 
-        Ok(())
+        Ok(backup_files)
     }
 
     pub fn prepare(&self) -> Result<()> {
